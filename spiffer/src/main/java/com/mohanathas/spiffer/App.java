212ad872b78ece5552471829c7df1fdaad55ca9b
Birunthan Mohanathas <birunthan@mohanathas.com>
2013-12-16 18:35:20 +0200

    Add initial NetBeans project

---
+++ spiffer/src/main/java/com/mohanathas/spiffer/App.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2013 Birunthan Mohanathas.
+ *
+ * Licensed under the MIT license <http://opensource.org/licenses/MIT>. This
+ * file may not be copied, modified, or distributed except according to those
+ * terms.
+ */
+
+package com.mohanathas.spiffer;
+
+/**
+ * Entry point.
+ *
+ */
+public class App 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
