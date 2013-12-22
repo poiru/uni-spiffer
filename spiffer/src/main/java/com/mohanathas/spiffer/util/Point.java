ec04b119fb90f8f8628da0bcdcce86a33b9b2c05
Birunthan Mohanathas <birunthan@mohanathas.com>
2013-12-22 20:51:09 +0000

    Add Point class

---
+++ spiffer/src/main/java/com/mohanathas/spiffer/util/Point.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2013 Birunthan Mohanathas
+ *
+ * Licensed under the MIT license <http://opensource.org/licenses/MIT>. This
+ * file may not be copied, modified, or distributed except according to those
+ * terms.
+ */
+
+package com.mohanathas.spiffer.util;
+
+/**
+ * Represents a location in (x, y) coordinate space.
+ */
+public class Point {
+    int mX;
+    int mY;
+
+    public Point(int x, int y) {
+        this.mX = x;
+        this.mY = y;
+    }
+
+    public int getX() {
+        return mX;
+    }
+
+    public int getY() {
+        return mY;
+    }
+}
