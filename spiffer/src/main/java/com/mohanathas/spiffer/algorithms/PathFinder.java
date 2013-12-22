9cc8405117851092b843306a8d0f4045ab7fb35f
Birunthan Mohanathas <birunthan@mohanathas.com>
2013-12-22 21:04:12 +0000

    Add PathFinder interface

---
+++ spiffer/src/main/java/com/mohanathas/spiffer/algorithms/PathFinder.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2013 Birunthan Mohanathas
+ *
+ * Licensed under the MIT license <http://opensource.org/licenses/MIT>. This
+ * file may not be copied, modified, or distributed except according to those
+ * terms.
+ */
+
+package com.mohanathas.spiffer.algorithms;
+
+import com.mohanathas.spiffer.util.Point;
+import java.util.List;
+
+/**
+ * Abstracts the path finding algorithm used by the Graph class.
+ */
+abstract interface PathFinder {
+    /**
+     * Finds a list of Points connecting the Nodes at |startPos| and |endPos|.
+     *
+     * @param graph Graph to search for the path.
+     * @param startNode Node to start the search from.
+     * @param endNode Node that is to be reached from |startNode|.
+     * @return List of Points if a path was found or |null| otherwise.
+     */
+    public List<Point> findPath(Graph graph, Node startNode, Node endNode);
+}
