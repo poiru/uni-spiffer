f62581bb8b72fbf00326e5e16d07afc6cdcfc927
Birunthan Mohanathas <birunthan@mohanathas.com>
2014-01-12 16:11:35 +0200

    Move common PathFinder tests into PathFinderTestBase

--- spiffer/src/test/java/com/mohanathas/spiffer/algorithm/JumpPointPathFinderTest.java
+++ spiffer/src/test/java/com/mohanathas/spiffer/algorithm/JumpPointPathFinderTest.java
@@ -8,74 +8,16 @@
 
 package com.mohanathas.spiffer.algorithm;
 
-import static com.mohanathas.spiffer.algorithm.AStarPathFinderTest.assertProcessedCountEquals;
 import com.mohanathas.spiffer.util.Point;
-import java.util.List;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
  * Tests the JumpPointPathFinder class.
  */
-public class JumpPointPathFinderTest {
-    private final JumpPointPathFinder mPathFinder = new JumpPointPathFinder(Heuristic.Chebyshev);
-
+public class JumpPointPathFinderTest extends PathFinderTestBase {
     public JumpPointPathFinderTest() {
-    }
-
-    void assertPathLengthEquals(float expected, Graph g, int x1, int y1, int x2, int y2) {
-        final Point start = new Point(x1, y1);
-        final List<Point> path = g.findPath(mPathFinder, start, new Point(x2, y2));
-        if (expected == 0.0f) {
-            assertNull(path);
-        } else {
-            assertNotNull(path);
-            assertEquals(expected, Graph.calculatePathLength(start, path), 0.01f);
-        }
-    }
-
-    @Test
-    public void testDirections() {
-        final Graph g = Graph.createFromIntArray(new int[][] {
-            {1, 1, 1},
-            {1, 1, 1},
-            {1, 1, 1}});
-        assertPathLengthEquals(1.0f, g, 1, 1, 0, 1);
-        assertPathLengthEquals(1.0f, g, 1, 1, 1, 0);
-        assertPathLengthEquals(1.0f, g, 1, 1, 2, 1);
-        assertPathLengthEquals(1.0f, g, 1, 1, 1, 2);
-    }
-
-    @Test
-    public void testValidPaths() {
-        final Graph g = Graph.createFromIntArray(new int[][] {
-            {1, 1, 1, 1},
-            {1, 1, 1, 1},
-            {1, 1, 1, 1}});
-        assertPathLengthEquals(3.0f, g, 0, 0, 3, 0);
-        assertPathLengthEquals(3.83f, g, 0, 0, 3, 2);
-    }
-
-    @Test
-    public void testValidPathsWithWalls() {
-        final Graph g = Graph.createFromIntArray(new int[][] {
-            {1, 0, 1, 1, 1},
-            {1, 1, 0, 0, 1},
-            {1, 0, 1, 1, 1},
-            {1, 1, 1, 0, 1}});
-        assertPathLengthEquals(9.66f, g, 0, 0, 2, 0);
-        assertPathLengthEquals(5.83f, g, 0, 0, 3, 2);
-        assertPathLengthEquals(7.24f, g, 0, 0, 4, 3);
-    }
-
-    @Test
-    public void testInvalidPaths() {
-        final Graph g = Graph.createFromIntArray(new int[][] {
-            {1, 0, 1},
-            {1, 0, 0},
-            {1, 1, 1}});
-        assertPathLengthEquals(0.0f, g, 0, 0, 2, 0);
-        assertPathLengthEquals(0.0f, g, 1, 2, 2, 0);
+        super(new JumpPointPathFinder(Heuristic.Chebyshev));
     }
 
     @Test
